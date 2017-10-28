550f603f3c34b5b9983645a4af15769eddfe4cc9
Winson <x@cother.club>
2017-10-28 11:42:34 +0800

    clear

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -14,17 +14,6 @@ export default {
 
   methods: {
     init() {
-      // dd.ready(function() {
-      //   dd.runtime.permission.requestAuthCode({
-      //     corpId: 'ding48e4d893b127a99935c2f4657eb6378f',
-      //     onSuccess: function(result) {
-      //       // alert(result.code);
-      //     },
-      //     onFail: function(err) {
-      //       console.log(JSON.stringify(err));
-      //     }
-      //   });
-      // });
     }
   }
 }
