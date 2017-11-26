935eb44a110a4119f88e3818c0f55f42d366dcdf
Cother <cother.xie@66ifuel.com>
2017-11-26 23:48:07 +0800

    'check'

--- src/pages/index/main.js
+++ src/pages/index/main.js
@@ -5,6 +5,7 @@ import router from '../../router'
 import cctb from '../../utils/index.js'
 import { config } from '../../config'
 
+
 import '../../assets/css/index.scss'
 
 window.ccConfig = config;
