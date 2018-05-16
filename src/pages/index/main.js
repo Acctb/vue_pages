334150ce7290e407503416641907989d2cc71c2d
Cother <x@cother.top>
2018-05-16 17:43:11 +0800

    pages

--- src/pages/index/main.js
+++ src/pages/index/main.js
@@ -1,12 +1,27 @@
 import Vue from 'vue'
-import App from './index'
 
+// request
+import ElementUI from 'element-ui'
+import locale from 'element-ui/lib/locale/lang/en' // lang i18n
+
+// style
+import 'normalize.css/normalize.css'// A modern alternative to CSS resets
+import '@/assets/styles/index.scss'
+import 'element-ui/lib/theme-chalk/index.css'
+
+// components
+import App from './index'
 import router from '../../router'
+import store from './store'
+
+import '@/icons' // icon
+import '@/permission' // permission control
+
+Vue.use(ElementUI, { locale })
+
+// config
 import cctb from '../../utils/index.js'
 import { config } from '../../config'
-
-import '../../assets/css/index.scss'
-
 window.ccConfig = config;
 window.cctb = cctb;
 
