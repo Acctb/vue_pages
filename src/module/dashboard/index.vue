c3c7108db2df2fc1ed2c061eb1d02ea978c8a05a
Cother <x@cother.top>
2018-05-16 18:09:28 +0800

    paegs

---
+++ src/module/dashboard/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="dashboard-container">
+    <div>123</div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'dashboard',
+  data() {
+  },
+  computed: {
+  },
+  created() {
+  }
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,31 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-
-import Main from '@/index/main'
+const _import = require('./_import_' + process.env.NODE_ENV)
 
 Vue.use(Router)
 
+export const constantRouterMap = [
+  {
+    path: '/',
+    component: Layout,
+    redirect: '/dashboard',
+    name: 'Dashboard',
+    hidden: true,
+    children: [{
+      path: 'dashboard',
+      component: () => import('@/module/dashboard/index')
+    }]
+  },
+  {
+    path: '/login',
+    component: () => import('@/module/login/index'),
+    hidden: true
+  }
+]
+
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'Main',
-      component: Main,
-      alias:'/main',
-    }
-  ]
+  // mode: 'history', //后端支持可开
+  scrollBehavior: () => ({ y: 0 }),
+  routes: constantRouterMap
 })
 
