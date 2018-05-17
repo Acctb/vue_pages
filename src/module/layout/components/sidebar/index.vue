ac4cf6751b8d63fd4d3dfdef0216db0e723c9d0c
Cother <x@cother.top>
2018-05-17 17:45:01 +0800

    pages

--- src/module/layout/components/sidebar/index.vue
+++ src/module/layout/components/sidebar/index.vue
@@ -1,10 +1,28 @@
 <template>
-  <div>
-  </div>
+  <el-scrollbar wrapClass="scrollbar-wrapper">
+    <el-menu mode="vertical" :show-timeout="200" :default-active="$route.path" :collapse="isCollapse" background-color="#304156" text-color="#bfcbd9" active-text-color="#409EFF">
+      <sidebar-item :routes="permission_routers"></sidebar-item>
+    </el-menu>
+  </el-scrollbar>
 </template>
 
 <script>
-export default {
-  name: 'Sidebar',
-}
+  import {
+    mapGetters
+  } from 'vuex'
+  import SidebarItem from './item'
+  export default {
+    components: {
+      SidebarItem
+    },
+    computed: {
+      ...mapGetters([
+        'permission_routers',
+        'sidebar'
+      ]),
+      isCollapse() {
+        return !this.sidebar.opened
+      }
+    }
+  }
 </script>
