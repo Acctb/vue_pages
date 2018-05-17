b9dbb5de48a47c95eb9db748f536c30780d4d6f1
Cother <x@cother.top>
2018-05-17 16:10:40 +0800

    pages

--- src/pages/index/main.js
+++ src/pages/index/main.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 
 // request
 import ElementUI from 'element-ui'
-import locale from 'element-ui/lib/locale/lang/en' // lang i18n
 
 // style
 import 'normalize.css/normalize.css'// A modern alternative to CSS resets
@@ -14,10 +13,14 @@ import App from './index'
 import router from '../../router'
 import store from '@/store'
 
+import i18n from '@/lang'
 import '@/icons' // icon
 import '@/permission' // permission control
 
-Vue.use(ElementUI, { locale })
+Vue.use(ElementUI, {
+  size: 'medium', // set element-ui default size
+  i18n: (key, value) => i18n.t(key, value)
+})
 
 // config
 import cctb from '../../utils/index.js'
@@ -31,6 +34,7 @@ new Vue({
   el: '#app',
   router,
   store,
+  i18n,
   template: '<App/>',
   components: { App }
 })
--- src/router/index.js
+++ src/router/index.js
@@ -30,6 +30,8 @@ export const constantRouterMap = [
   { path: '*', redirect: '/404', hidden: true }
 ]
 
+export const asyncRouterMap = []
+
 export default new Router({
   // mode: 'history', //后端支持可开
   scrollBehavior: () => ({ y: 0 }),
