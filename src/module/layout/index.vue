0597dbabb18a43aff7df604a135792510d9c6356
Cother <x@cother.top>
2018-05-18 22:26:54 +0800

    pages

--- src/module/layout/index.vue
+++ src/module/layout/index.vue
@@ -17,6 +17,7 @@
     Container,
     Tags
   } from './components'
+  import ResizeMixin from './mixin/ResizeHandler'
   export default {
     name: 'layout',
     components: {
@@ -25,6 +26,29 @@
       Container,
       Tags
     },
+    mixins: [ResizeMixin],
+    computed: {
+      sidebar() {
+        return this.$store.state.app.sidebar
+      },
+      device() {
+        return this.$store.state.app.device
+      },
+      classObj() {
+        return {
+          hideSidebar: !this.sidebar.opened,
+          withoutAnimation: this.sidebar.withoutAnimation,
+          mobile: this.device === 'mobile'
+        }
+      }
+    },
+    methods: {
+      handleClickOutside() {
+        this.$store.dispatch('closeSideBar', {
+          withoutAnimation: false
+        })
+      }
+    }
   }
 </script>
 <style lang="scss" scoped>
